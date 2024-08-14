0e0a6c294e129ab98bdaa9a3cee10a92317baccd
tristeceratops <eric.woillard@gmail.com>
2024-08-14 15:42:18 +0200

    fix test string vide

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ewoillar <ewoillar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/12 17:38:40 by ewoillar          #+#    #+#             */
-/*   Updated: 2024/08/14 15:10:43 by ewoillar         ###   ########.fr       */
+/*   Updated: 2024/08/14 15:41:03 by ewoillar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,9 @@ int main(int argc, char **argv)
 {
 	if (argc != 4)
 		return (std::cerr << "Error: must be ./sed <filename> <find> <replace>" << std::endl, 1);
-	if (!argv[1] || !argv[2] || !argv[3])
+	if (std::string(argv[1]).empty() || 
+        std::string(argv[2]).empty() || 
+        std::string(argv[3]).empty())
 		return (std::cerr << "Value can't be empty string!" << std::endl, 1);
 	std::fstream file;
 	std::fstream file_replace;
@@ -30,10 +32,10 @@ int main(int argc, char **argv)
 	std::string replace_filename = filename + ".replace";
 	file.open(argv[1], std::ios::in);
 	if (!file.is_open())
-		return (std::cerr << "Error: file not found" << std::endl, 1);
+		return (std::cerr << "Error: file not found or insufficient permissions to read the file." << std::endl, 1);
 	file_replace.open(replace_filename.c_str(), std::ios::out);
 	if (!file_replace.is_open())
-		return (std::cerr << "Error: could not create file" << std::endl, file.close(), 1);
+		return (std::cerr << "Error: could not create or write in file. Check if you have write permissions." << std::endl, file.close(), 1);
 	while (getline(file, line))
 	{
 		std::string new_line;
