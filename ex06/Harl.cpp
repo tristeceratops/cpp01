c4aa5a11dc213d4aa8aa905c4e4e4d74000fac0a
tristeceratops <eric.woillard@gmail.com>
2024-08-16 14:00:24 +0200

    ex06 ok

---
+++ ex06/Harl.cpp
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ewoillar <ewoillar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/14 15:43:42 by ewoillar          #+#    #+#             */
+/*   Updated: 2024/08/16 13:59:47 by ewoillar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+Harl::Harl()
+{
+}
+
+Harl::~Harl()
+{
+}
+
+void Harl::complain(std::string level)
+{
+	int index = -1;
+	Harl harl;
+	pointToFunc p[] = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+	std::string l[] = {"debug", "info", "warning", "error"};
+	for(int i = 0; i < 4; i++)
+	{
+		if (level == l[i])
+		{
+			index = i;
+			break;
+		}
+	}
+	switch (index)
+	{
+		case 0:
+			(harl.*p[0])();
+		case 1:
+			(harl.*p[1])();
+		case 2:
+			(harl.*p[2])();
+		case 3:
+			(harl.*p[3])();
+			break;		
+		default:
+			break;
+	}
+}
+
+void Harl::debug()
+{
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+}
+
+void Harl::info()
+{
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!" << std::endl;	
+}
+
+void Harl::warning()
+{
+	std::cout << "I think I deserve to have some extra bacon for free. I've been coming foryears whereas you started working here since last month." << std::endl;
+}
+
+void Harl::error()
+{
+	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+}
---
+++ ex06/Harl.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ewoillar <ewoillar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/14 15:43:45 by ewoillar          #+#    #+#             */
+/*   Updated: 2024/08/16 12:53:48 by ewoillar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include <string>
+#include <iostream>
+
+class Harl
+{
+	public:
+		Harl();
+		~Harl();
+		void	complain(std::string level);
+	private:
+		void debug();
+		void info();
+		void warning();
+		void error();
+};
+
+typedef void(Harl::*pointToFunc)(void);
