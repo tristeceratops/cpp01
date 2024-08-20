c6c2c0c9a0a2fab147da33e543f7347711175da3
tristeceratops <eric.woillard@gmail.com>
2024-08-20 14:08:35 +0200

    fix ex01

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,16 +6,19 @@
 /*   By: ewoillar <ewoillar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/09 16:07:54 by ewoillar          #+#    #+#             */
-/*   Updated: 2024/08/12 13:15:56 by ewoillar         ###   ########.fr       */
+/*   Updated: 2024/08/20 14:08:17 by ewoillar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
+#include <limits.h>
 
 int main()
 {
-	int N = 4;
+	int N = -4;
 	Zombie *horde = zombieHorde(N, "René");
+	if (!horde)
+		return (1);
 	for(int i = 0; i < N; i++)
 		horde[i].announce();
 	delete[] horde;
--- ex01/zombieHorde.cpp
+++ ex01/zombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: ewoillar <ewoillar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/12 12:46:57 by ewoillar          #+#    #+#             */
-/*   Updated: 2024/08/12 13:10:54 by ewoillar         ###   ########.fr       */
+/*   Updated: 2024/08/20 14:08:04 by ewoillar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,19 @@
 
 Zombie* zombieHorde(int N, std::string name)
 {
-	Zombie *horde = new Zombie[N];
-	for (int i = 0; i < N; i++)
-		horde[i].setName(name);
-	return (horde);
+	if (N <= 0)
+	{
+		std::cerr << "N must be positive" << std::endl;
+		return NULL;
+	}
+	try
+	{
+		Zombie *horde = new Zombie[N];
+		for (int i = 0; i < N; i++)
+			horde[i].setName(name);
+		return (horde);
+	} catch (const std::bad_alloc& e) {
+		std::cerr << "Allocation failed: " << e.what() << std::endl;
+		return NULL;
+	}
 }
